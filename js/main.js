3ca3db03f8808dfb6b0d0000d1ccf08e015619cf
BizheOrkhan <orkhan.b@labrin.tech>
2020-04-22 13:08:38 +0400

    features section ready

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    
+
+    $(window).scroll(function () { 
+        if($("html").scrollTop() >= 45){
+            $("#navigation .navbar").addClass("scrolled");
+        }else{
+            $("#navigation .navbar").removeClass("scrolled");
+        }
+    });
+
+});
