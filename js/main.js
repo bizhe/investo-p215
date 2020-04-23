b03aed2e96afb6584c9f52c441f9f530c8cd98db
BizheOrkhan <orkhan.b@labrin.tech>
2020-04-23 13:44:30 +0400

    client section done

--- js/main.js
+++ js/main.js
@@ -19,4 +19,35 @@ $(document).ready(function () {
         $(this).find(".dropdown-menu").removeClass("show");
    })
 
+
+   // client says slider
+
+   if($(".items .item").length){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:20,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+        responsiveClass:true,
+        nav: false, 
+        dots: false,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            600:{
+                items:2,
+                nav:false
+            },
+            1000:{
+                items:2,
+            }
+        }
+    })
+   }
+
+   
+
 });
